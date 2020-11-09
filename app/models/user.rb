e3968e7a3c37d490d5b6159771d15e13c41adccb
Hara Tomoka <pixieyunomi@gmail.com>
2020-11-09 20:10:21 +0900

    Change guest name with chinese character

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.name = 'read-only-user'
+      user.name = '原朋伽'
     end
   end
 
