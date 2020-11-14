c3ac7e84e036350076c5ac2ab73702d99130ef38
Hara Tomoka <pixieyunomi@gmail.com>
2020-11-14 14:23:42 +0900

    Modify edit_lessons to update in any case

--- app/lib/batch/edit_lessons.rb
+++ app/lib/batch/edit_lessons.rb
@@ -2,10 +2,13 @@ class Batch::EditLessons
   def self.edit_lessons
     # 投稿を全て削除
     lessons = Lesson.all
-
+    # 時刻を日本時間に戻す
+    now = Time.zone.now
     lessons.each do |lesson|
-      if lesson.max_attendees <= lesson.reservations.count || lesson.deadline < DateTime.now
-        lesson.update(is_active: false)
+      if lesson.max_attendees <= lesson.reservations.count || lesson.deadline < now
+        lesson.is_active = false
+        # 強制的にfalseに変える
+        lesson.save!(validate: false)
         p "予約不可能なレッスンを削除しました。"
       end
     end
