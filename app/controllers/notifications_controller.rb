46a7cf852fe0702da2a49f509509684f8d9dbfd6
Hara Tomoka <pixieyunomi@gmail.com>
2020-11-22 15:59:02 +0900

    Change production.rb true to false

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,10 +2,15 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
   def index
     # current_userの投稿に紐づいた通知一覧
-    @notifications = current_user.passive_notifications
+    all_notifications = current_user.passive_notifications
     # @notificationの中でまだ確認していない(indexに一度も遷移していない)通知のみ
-    @notifications.where(is_checked: false).each do |notification|
+    all_notifications.where(is_checked: false).each do |notification|
       notification.update_attributes(is_checked: true)
+    end
+    @notifications = all_notifications.page(params[:page]).reverse_order
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
