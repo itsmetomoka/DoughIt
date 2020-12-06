6e3b9a13b07df1513665f39980f10db2e1e24d05
Hara Tomoka <pixieyunomi@gmail.com>
2020-12-06 18:03:46 +0900

    replace resized image url with helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	def non_active_user_image
+		"https://pf-doughit-resize.s3-ap-northeast-1.amazonaws.com/uploads/user/image/no-user.jpg"
+	end
+
+	def no_user_image_url
+		"https://pf-doughit-resize.s3-ap-northeast-1.amazonaws.com/uploads/user/image/noimage.png"
+	end
+
+	def user_image_url(user)
+		"https://pf-doughit-resize.s3-ap-northeast-1.amazonaws.com/uploads/user/image/" + user.id.to_s + '/' + user.image_file_name
+	end
 end
--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,2 +1,6 @@
 module LessonsHelper
+
+	def s3_lesson_image(lesson)
+		lesson_image = "https://pf-doughit-resize.s3-ap-northeast-1.amazonaws.com/uploads/lesson/image/" +lesson.id.to_s + '/' + lesson.image_file_name
+	end
 end
